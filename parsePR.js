0f4132891b33577039e310219d6fa801930b57c9
Manuel Villen <manuvillen@gmail.com>
2024-05-27 20:16:53 +0200

    Add --array-flag

--- parsePR.js
+++ parsePR.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
+const fs = require('fs')
 const readline = require('readline')
 
 async function extractTests(){
 
     //by default we specify that all tests should run
-    let testsFile = __dirname+'/testsToRun.txt';
-    await fs.promises.writeFile(testsFile,'all');
+    let testsFile = __dirname+'/testsToRun.txt'
+    await fs.promises.writeFile(testsFile,'all')
 
     const lines = readline.createInterface({
         input: fs.createReadStream(__dirname+'/pr_body.txt'),
@@ -16,9 +16,10 @@ async function extractTests(){
         //special delimeter for apex tests
         if(line.includes('Apex::[') && line.includes(']::Apex')){
 
-            let tests = line.substring(8,line.length-7);
-            await fs.promises.writeFile(testsFile,tests);
-            await fs.promises.appendFile(testsFile,'\n');
+            let tests = line.substring(8,line.length-7)
+            tests = ' --array-flag ' + tests
+            await fs.promises.writeFile(testsFile,tests)
+            await fs.promises.appendFile(testsFile,' ')
         }
     }
 }
